e06823cdc6297f61f7660f56fb4193710db072dd
mahbube <mahbub.khazaee@gmail.com>
2020-09-29 10:53:10 -0700

    Delete extra fields

--- src/app/users/admin/admin.ts
+++
@@ -1,6 +0,0 @@
-import {User, UserRoll} from '../../core/model/user';
-
-export class Admin extends User{
-password='12345678';
-    roll:UserRoll.ADMIN;
-}
--- src/app/users/teams-list/team.ts
+++
@@ -1,8 +0,0 @@
-import {User, UserRoll} from '../../core/model/user';
-
-export class Team extends User{
-    password = '123456';
-    roll = UserRoll.TEAM;
-    members:string;
-  centerId:string;
-}
